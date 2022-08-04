bb01db48fb8d5eef33f430f5fedbaabc3400a327
Daniel <daniel@kylix.online>
2022-08-04 12:51:41 +0300

    new changes

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const gifts = ["Guadalupe", "Ollie", "Aki"];
+const messages = []
+const writeCards = (array,event) => {
+for (let counter = 0; counter < array.length; counter++) {
+    let message = `Thank you, ${array[counter]}, for the wonderful ${event} gift!`
+    messages.push(message)
+    return messages
+}
+}
+
+writeCards(gifts,'surprise')
+const countDown = (countdown) => {
+    while (countdown > -1) {
+        console.log(countdown)
+        countdown--;
+    }
+
+}
+countDown(10)
